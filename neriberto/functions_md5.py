630f8d12c4c4f776e6c33b08cd53ddfc9e45aa19
Neriberto C.Prado <neriberto.prado@bluepex.com>
2015-02-19 14:49:25 -0200

    Adding header to functions_md5.py

--- neriberto/functions_md5.py
+++ neriberto/functions_md5.py
@@ -1,3 +1,10 @@
+#!/usr/bin/env python
+
+"""functions_md5.py: Enumerate functions and hash it."""
+
+__author__	= "Neriberto C.Prado"
+__copyright__	= "Copyright 2015, Neriberto C.Prado"
+
 import hashlib
 
 for fn_ea in Functions():
